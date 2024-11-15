a2214a39820163dbdfae5ad1f6c2e751f5442d22
Oliver Thim <oliver.thim@chalmers.se>
2024-11-15 09:17:39 +0100

    Cleaned up antisym_3N.py

--- src/states/antisym_3N.py
+++ src/states/antisym_3N.py
@@ -26,7 +26,6 @@ from operator import itemgetter
 def hat(j):
     return np.sqrt(2*j+1)
 
-
 def get_hat_factors(bra,ket,L,S2):
     '''
     Args:
@@ -39,7 +38,6 @@ def get_hat_factors(bra,ket,L,S2):
     Returns:
         All hat-factors in eq. (XX) for the given bra and ket quantum numbers
     '''
-
 
     return hat(bra['t'])*hat(ket['t'])*(hat(L)**2)*(hat(S2/2.0)**2)*hat(bra['j'])*hat(ket['j'])*\
                 hat(bra['cJ2']/2.0)*hat(ket['cJ2']/2.0)*hat(bra['s'])*hat(ket['s'])
@@ -89,26 +87,13 @@ def get_6j_factors(bra,ket,L,S2):
     
     facS = wig.wig6jj(int(2*1/2),  int(2*1/2), 2*bra['s'],\
                       int(2*1/2),  S2,   2*ket['s'])
-    #print(f'facT={facT}, facS={facS}')
+    
     return facT*facS
 
-def get_HO_braket(bra,ket,L,S2):
+def setup_3N_antisymmetrizer_element(bra,ket,verbose=False):
     '''
-    Args:
-        bra (dictionary(n,l,s,j,t,mt,pi,cN,cL,cJ2,J2,T2)) : bra-quantum state
-        as defined in basis_states.py.
-
-        ket (dictionary(n,l,s,j,t,mt,pi,cN,cL,cJ2,J2,T2)) : ket-quantum state
-        L  (int) : coupled total L 
-        S2 (int) : 2*S, where S is the total coupled spin
-    Returns:
-        The general HO braket
-    '''
-
-def setup_3N_antisymmetrizer_element_2(bra,ket,verbose=False):
-    '''
-        Computes the antisymmetrizer matrix elements as defined in eq. (XX) 
-        in the notes.
+        Computes the antisymmetrizer matrix elements as defined in 
+        Eq. (31) in Phys. Rev. C 61 044001.
 
     Args:
 
@@ -140,30 +125,95 @@ def setup_3N_antisymmetrizer_element_2(bra,ket,verbose=False):
             # Mass ratio
             d=1/3.0
 
-            # *********************************************
+            # ***********************************************
             # Arguments:
             # (n,l,nc,lc,n1,l1,n2,l2,lr,d) 
             # All are (int) except d which is a float.
             # For documentation, see the file my_braket.f90
-            # *********************************************
+            # This bracket is defined as in Trjilfaj (1972)
+            # but gmosh takes '1/d' as argument, hence the
+            # argument passed here is 1/3 which differs
+            # compared to Eq. (31) in Phys. Rev. C 61 044001.
+            # More details of the implementation is provided
+            # in the report.
+            # ***********************************************
             fac5 = gmosh.angmom_lib.gmosh(bra['n'],bra['l'],\
                     bra['cN'],bra['cL'],ket['cN'],ket['cL'],\
                     ket['n'],ket['l'],L,d)
             
             matrix_el += fac1*fac2*fac3*fac4*fac5
-            #print(f'S2={S2}, LL={LL}')
-            #print(f'fac1={fac1}')
-            #print(f'fac2={fac2}')
-            #print(f'9j={fac3}')
-            #print(f'6j={fac4}')
-            #print(f'gmosh={fac5}\n')
 
     
-    #print(f'matrix_el={matrix_el}\n')
-
     return matrix_el
 
-def setup_3N_antisymmetrizer_element(bra,ket,verbose=False):
+def check_A(A):
+    '''
+        Funtion that returns true if A is a projector.
+    '''
+
+    if (np.max(np.abs(A-A@A))>1e-6):
+        return False
+    else:
+        return True
+
+def check_eigs(eigs):
+    '''
+        Function that checks if all eigenvalues are either zero or one.
+    '''
+
+    num1 = np.sum([np.abs(e-1.0)<1e-6 for e in eigs])
+    num2 = np.sum([np.abs(e-0.0)<1e-6 for e in eigs])
+    if (num1+num2 != len(eigs)):
+        print(eigs)
+        return False
+    else:
+        return True
+
+def setup_3N_antisymmetrizer_matrix(basis,verbose=False):
+    '''
+        Computes the antisymmetrizer matrix <bra|A|ket>.
+
+    Args:
+
+        basis (list) : list of basis states as computed with 
+                       basis_states.NN_basis_nl()
+    Returns:
+
+        A (np.array(len(basis),len(basis)))
+    '''
+       
+    A = np.zeros((len(basis),len(basis)))
+    
+    for i,bra in enumerate(basis):
+        for j,ket in enumerate(basis):
+
+            if verbose:
+                print(f'\ni={i}, j={j}')   
+            
+            braN = 2*bra['n']+bra['l'] + 2*bra['cN']+bra['cL']
+            ketN = 2*ket['n']+ket['l'] + 2*ket['cN']+ket['cL']
+
+            matrix_el = 0
+            # Check that (N,J,T) are the same for the states
+            if (braN == ketN and bra['J2'] == ket['J2'] and bra['T2'] == ket['T2']): 
+                matrix_el = setup_3N_antisymmetrizer_element(bra,ket,verbose)
+                #matrix_el_fortran = tm.antisym_me_mymod_braket(bra,ket)
+            else:
+                matrix_el = 0
+
+            # Set calculated matrix element
+            A[i,j] = (-2.0/3.0)*matrix_el
+            if (i==j):
+                A[i,j] += 1.0/3.0
+            
+    if verbose:
+        print(f'A.shape={A.shape}')
+    return A
+
+# *****************************************************************************
+# OLD function
+# *****************************************************************************
+def setup_3N_antisymmetrizer_element_2(bra,ket,verbose=False):
     '''
         Computes the antisymmetrizer matrix elements as defined in eq. (XX) 
         in the notes.
@@ -178,6 +228,7 @@ def setup_3N_antisymmetrizer_element(bra,ket,verbose=False):
     Returns:
         matrix_el (float) 
     '''
+    CASE = 0
     matrix_el = 0
 
     print(f'\nGetting A element')
@@ -282,118 +333,3 @@ def setup_3N_antisymmetrizer_element(bra,ket,verbose=False):
     print(f'matrix_el={matrix_el}')
 
     return matrix_el
-
-def check_A(A):
-
-    if (np.max(np.abs(A-A@A))>1e-6):
-        return False
-    else:
-        return True
-
-def check_eigs(eigs):
-
-    num1 = np.sum([np.abs(e-1.0)<1e-6 for e in eigs])
-    num2 = np.sum([np.abs(e-0.0)<1e-6 for e in eigs])
-    if (num1+num2 != len(eigs)):
-        print(eigs)
-        return False
-    else:
-        return True
-
-def setup_3N_antisymmetrizer_matrix(basis,verbose=False):
-    '''
-        Computes the antisymmetrizer matrix as defined in eq. (XX) in the notes.
-
-    Args:
-
-        basis (list) : list of basis states as computed with 
-                       basis_states.NN_basis_nl()
-        j2max (int)  : 2*j_max for the wigner symbols
-
-    Returns:
-        antisym_matrix (np.array(len(basis),len(basis)))
-    '''
-       
-    A = np.zeros((len(basis),len(basis)))
-    
-    for i,bra in enumerate(basis):
-        for j,ket in enumerate(basis):
-            #if i!=1 or j!=1: continue
-
-            #print(f'bra {bra}')
-            #print(f'ket {ket}')
-            if verbose:
-                print(f'\ni={i}, j={j}')   
-            
-            braN = 2*bra['n']+bra['l'] + 2*bra['cN']+bra['cL']
-            ketN = 2*ket['n']+ket['l'] + 2*ket['cN']+ket['cL']
-
-            matrix_el = 0
-            # Check that (N,J,T) are the same for the states
-            if (braN == ketN and bra['J2'] == ket['J2'] and bra['T2'] == ket['T2']): 
-                matrix_el = setup_3N_antisymmetrizer_element_2(bra,ket,verbose)
-                #matrix_el_fortran = tm.antisym_me_mymod_braket(bra,ket)
-                #print("ME:")
-                #print(matrix_el)
-                #print((-3/2)*matrix_el_fortran)
-                #if (abs(matrix_el - (-3/2)*matrix_el_fortran)>1e-6):
-                #    print("Error")
-                #    print(bra)
-                #    print(ket)
-                    #input(x)
-                #matrix_el = matrix_el_fortran*(-3/2)
-            else:
-                #print('Error, states are not grouped in (N,J,T)')
-                matrix_el = 0
-
-            # Set calculated matrix element
-            A[i,j] = (-2.0/3.0)*matrix_el
-            if (i==j):
-                A[i,j] += 1.0/3.0
-            
-    if verbose:
-        print(f'A.shape={A.shape}')
-    return A
-
-#def compute_alpha_gamma_matrix():
-
-
-
-CASE = 0
-if __name__=="__main__":
-    
-    gmosh.angmom_lib.precalculate_binomials() 
-    gmosh.factorials.precalculate_factorials() 
-    gmosh.angmom_lib.setup_moshinsky() 
-    # Setup wigner symbols
-    wig.wig_table_init(2*100, 9)
-    wig.wig_temp_init(2*100)
-    # Construct a basis
-    Nmax = 4
-    states = bs.NNN_basis_nl(Nmax,True)
-
-    # Group states with same N, J2 and T2
-    grouper = itemgetter("N","J2", "T2") 
-    #grouper = itemgetter("J2","T2", "pi") 
-    NJT_list = bs.group_NNN_basis_nl(grouper, states, verbose=True)
-
-    # Compute antisummetrizer matrix
-    for i,njtlist in enumerate(NJT_list):
-        #if i!=9: continue
-        print(f'\n\ni={i}')
-        A1 = setup_3N_antisymmetrizer_matrix(njtlist,False)
-        #print(A1)
-        np.set_printoptions(precision=1)
-        #print(f'A')
-        #for i in range(len(A1[0,:])):
-        #    print(A1[i,:])
-
-        eigs,eigv = np.linalg.eig(A1)
-        print(f'eigs={eigs}')
-        print(f'max |A1-A1^2| = {np.max(np.abs(A1-A1@A1))}')
-        print(f'# eigs=1: {np.sum([np.abs(e-1.0)<1e-10 for e in eigs])}')
-        print(f'# eigs=0: {np.sum([np.abs(e-0.0)<1e-10 for e in eigs])}')
-        print(f'len(eigs)={len(eigs)}')
-
-    
-    
